1312c2db816642a9c52d19d8775f922c8d2c9425
HorizUntall <rayuntal0112@gmail.com>
2026-02-02 16:49:07 +0800

    priority modal

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // For priority modal
 const priorityBtn = document.getElementById("priorityBtn");
 const priorityModal = document.getElementById("priorityModal");
+const closeModalBtn = document.getElementById("closeModalBtn");
 
 priorityBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -9,4 +10,22 @@ priorityBtn.addEventListener("click", (e) => {
   } else {
     priorityModal.showModal();
   }
+});
+
+function closeModal() {
+  setTimeout(() => {
+    priorityModal.close();
+  }, 150);
+
+  console.log(document.getElementById);
+}
+
+// Close the modal when the "X" is clicked
+closeModalBtn.addEventListener("click", () => {
+  priorityModal.close();
+});
+
+// Also keep your form logic to close on selection
+priorityForm.addEventListener("change", () => {
+  priorityModal.close();
 });
